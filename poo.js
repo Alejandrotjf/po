b89324144275a0fd4d49aa36fc1a0c1a0f1bf04e
Alejandro.Angeles@axity.com <Alejandro.Angeles@axity.com>
2025-02-10 10:20:36 -0600

    poo

---
+++ poo.js
@@ -0,0 +1,23 @@
+var Oscar = /** @class */ (function () {
+    function Oscar() {
+        this.name = "oscar";
+        this.sex = "macho";
+        this.age = 3;
+        this.weight = 7;
+        this.color = "marron";
+        this.textura = "rayada";
+    }
+    return Oscar;
+}());
+var Luna = /** @class */ (function () {
+    function Luna() {
+        this.name = "luna";
+        this.sex = "hembra";
+        this.age = 2;
+        this.weight = 5;
+        this.color = "gris";
+        this.textura = "lisa";
+    }
+    return Luna;
+}());
+console.log("hola");
---
+++ poo.ts
@@ -0,0 +1,19 @@
+class Oscar{
+    name:string="oscar"
+    sex:string="macho"
+    age:number=3
+    weight:number=7
+    color:string="marron"
+    textura:string="rayada"
+}
+
+class Luna{
+    name:string="luna"
+    sex:string="hembra"
+    age:number=2
+    weight:number=5
+    color:string="gris"
+    textura:string="lisa"
+}
+
+console.log("hola")
